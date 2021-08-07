20ccb320f246ecde6df44822fa819ffd297f7cd2
tijs <tijs@feka.nl>
2021-08-07 15:19:44 +0200

    Hercules: added find in inventory utility

--- Hesperides/hercules.lua
+++ Hesperides/hercules.lua
@@ -4,8 +4,18 @@ print("1 juicer in inventory")
 
 local TOTAL_SLOTS = 16
 
-for i = 1, TOTAL_SLOTS, 1 do
-    print("Slot" .. i)
+function findInPocket (toFind)
+    local found = false
+    for slot = 1, TOTAL_SLOTS, 1 do
+        if turtle.getItemCount(slot) > 0 do
+            local item = turtle.getItemDetail(slot) 
+            found = item.name == toFind
+        end
+
+        if found then 
+            break
+        end
+    end
 end
 
-assert()
+assert(findInPocket("pamhc2foodcord:juiceritem"))
