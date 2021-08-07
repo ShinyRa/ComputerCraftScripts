a802cb9a659aabc4c9fc4f11385a5fa6d317017e
tijs <tijs@feka.nl>
2021-08-07 18:08:09 +0200

    Installer fixed copy to startup.lua

--- installer.lua
+++ installer.lua
@@ -9,14 +9,15 @@ if string.lower(label) == "gaia" then
     print("Downloading files")
     shell.run("hercules")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/gaia.lua")
-    shell.run("cp gaia ../rom/startup/autorun.lua")
+    shell.run("cp gaia ../rom/autorun/startup.lua")
     shell.run("gaia")
 elseif string.lower(label) == "hercules" then
     print("Installation for Hercules")
     print("Downloading files")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
-    shell.run("cp hercules ../rom/startup/autorun.lua")
+    shell.run("cp hercules ../rom/autorun/startup.lua")
     shell.run("hercules")
 end
 
+shell.setDir("../")
 fs.delete("installer.lua")
