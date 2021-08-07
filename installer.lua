f0b782041bf6ac56a5b765dccc0433e801e68179
tijs <tijs@feka.nl>
2021-08-07 18:04:28 +0200

    Fixed string lowercase in installer

--- installer.lua
+++ installer.lua
@@ -4,14 +4,14 @@ fs.makeDir("pulled")
 shell.setDir("pulled/")
 local label = os.getComputerLabel()
 
-if string.lower(label) == "Gaia" then
+if string.lower(label) == "gaia" then
     print("Installation for Gaia")
     print("Downloading files")
     shell.run("hercules")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/gaia.lua")
     shell.run("cp gaia ../rom/startup/autorun.lua")
     shell.run("gaia")
-elseif string.lower(label) == "Hercules" then
+elseif string.lower(label) == "hercules" then
     print("Installation for Hercules")
     print("Downloading files")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
