37532bf8830d5e625e65517a2870f190bc18a171
tijs <tijs@feka.nl>
2021-08-07 18:15:22 +0200

    Changed startup location

--- installer.lua
+++ installer.lua
@@ -7,16 +7,15 @@ local label = os.getComputerLabel()
 if string.lower(label) == "gaia" then
     print("Installation for Gaia")
     print("Downloading files")
-    shell.run("hercules")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/gaia.lua")
-    shell.run("cp gaia ../rom/autorun/startup.lua")
+    shell.run("cp gaia.lua ../startup.lua")
 elseif string.lower(label) == "hercules" then
     print("Installation for Hercules")
     print("Downloading files")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
-    shell.run("cp hercules ../rom/autorun/startup.lua")
+    shell.run("cp hercules.lua ../startup.lua")
 end
 
-shell.setDir("../")
+shell.setDir("/")
 fs.delete("installer.lua")
 shell.run("reboot")
