951ae46273f82e25260fe940602c09bf75c4e44a
tijs <tijs@feka.nl>
2021-08-07 17:59:16 +0200

    Extended installation script

--- installer.lua
+++ installer.lua
@@ -1,9 +1,22 @@
 print("Removing junk")
-fs.delete("startup.lua")
-fs.delete("hercules.lua")
-print("Installing hercules...")
-print("Downloading files")
-shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
-print("Running hercules")
-shell.run("hercules")
+fs.delete("pulled/")
+fs.makeDir("pulled")
+shell.setDir("pulled/")
+local label = os.getComputerLabel()
+
+if label == "Gaia" then
+    print("Installation for Gaia")
+    print("Downloading files")
+    shell.run("hercules")
+    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/gaia.lua")
+    shell.run("cp gaia ../rom/startup/autorun.lua")
+    shell.run("gaia")
+elseif label == "Hercules" then
+    print("Installation for Hercules")
+    print("Downloading files")
+    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
+    shell.run("cp hercules ../rom/startup/autorun.lua")
+    shell.run("hercules")
+end
+
 fs.delete("installer.lua")
