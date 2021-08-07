82417fb26c5ab40d530a04a9f9ee57ccea4e4c77
tijs <tijs@feka.nl>
2021-08-07 18:09:45 +0200

    Reboot instead of running programme

--- installer.lua
+++ installer.lua
@@ -10,14 +10,13 @@ if string.lower(label) == "gaia" then
     shell.run("hercules")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/gaia.lua")
     shell.run("cp gaia ../rom/autorun/startup.lua")
-    shell.run("gaia")
 elseif string.lower(label) == "hercules" then
     print("Installation for Hercules")
     print("Downloading files")
     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/hercules.lua")
     shell.run("cp hercules ../rom/autorun/startup.lua")
-    shell.run("hercules")
 end
 
 shell.setDir("../")
 fs.delete("installer.lua")
+shell.run("reboot")
