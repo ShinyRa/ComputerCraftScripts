9a032fe6b92aa3ed43178f84e7c69c3b57556ef4
tijs <tijs@feka.nl>
2021-08-07 17:01:11 +0200

    Hercules: crafting and depositing fruit punch alogorithm

--- Hesperides/hercules.lua
+++ Hesperides/hercules.lua
@@ -3,8 +3,10 @@ print("Hercules prerequisites:")
 print("1 juicer in inventory")
 
 local TOTAL_SLOTS = 16
+local JUICE_CRAFTING_SLOTS = {2, 5, 6}
 
 local juicerItemTag = "pamhc2foodcore:juiceritem"
+local fruitPunchItemTag = "pamhc2foodcore:fruitpunchitem"
 local obsidianChestTag = "expandedstorage:obsidian_chest"
 local regularChestTag = "minecraft:chest"
 
@@ -13,11 +15,11 @@ local regularChestTag = "minecraft:chest"
 function findInPocket(toFind)
     for slot = 1, TOTAL_SLOTS, 1 do
         if turtle.getItemCount(slot) > 0 then
-            turtle.select(slot)
             local item = turtle.getItemDetail(slot)
+            turtle.select(slot)
             if item.name == toFind then
                 turtle.transferTo(1)
-                return true
+                return turtle.getSelectedSlot()
             end
         end
     end
@@ -63,16 +65,26 @@ end
 
 -- Helper function craft item
 function craftJuice()
-    assert(findInPocket(juicerItemTag), "Required juicer not found in Hercules' inventory")
+    assert(findInPocket(juicerItemTag) > 0, "Required juicer not found in Hercules' inventory")
     findMelonChest()
-    turtle.select(2)
-    turtle.suck()
+
+    for slot = 1, #JUICE_CRAFTING_SLOTS do
+        turtle.select(JUICE_CRAFTING_SLOTSs[slot])
+        turtle.suck()
+    end
+
+    turtle.craft()
+end
+
+function depositJuice()
+    local fruitJuiceSlot = findInPocket(fruitPunchItemTag)
+    if fruitJuiceSlot > 0 then
+        findOutputChest()
+        turtle.select(fruitJuiceSlot)
+        turtle.dropDown()
+    end 
 end
 
 craftJuice()
--- while true do
-    
-
--- end
-
+depositJuice()
 
