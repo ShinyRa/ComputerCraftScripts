cc1e9fb3a3e9a71e9eb4d55718ba49b0cd7ed55c
tijs <tijs@feka.nl>
2021-08-12 00:32:00 +0200

    Added interactable menu to installer

--- installer.lua
+++ installer.lua
@@ -1,4 +1,5 @@
-local url = "https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main"
+local pretty = require("cc.pretty")
+local repository = "https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/"
 
 local installationTree = {
     ["Herperides"] = {
@@ -9,7 +10,7 @@ local installationTree = {
                 type = "Computer",
                 peripherals = "[♬, ⌧*]",
                 files = {
-                    "apollo.lua"
+                    "Herperides/apollo.lua"
                 }
             },
             ["Ares"] = {
@@ -17,7 +18,7 @@ local installationTree = {
                 type = "Turtle (⚔)",
                 peripherals = "[]",
                 files = {
-                    "ares.lua"
+                    "Herperides/ares.lua"
                 }
             },
             ["Gaia"] = {
@@ -25,10 +26,10 @@ local installationTree = {
                 type = "Computer",
                 peripherals = "[♬, ⛀, ⌧]",
                 files = {
-                    "gaia.lua",
-                    "images/logo.nfp",
-                    "images/logo2.nfp",
-                    "images/punch.nfp"
+                    "Herperides/gaia.lua",
+                    "Herperides/images/logo.nfp",
+                    "Herperides/images/logo2.nfp",
+                    "Herperides/images/punch.nfp"
                 }
             },
             ["Hercules"] = {
@@ -36,7 +37,7 @@ local installationTree = {
                 type = "Computer",
                 peripherals = "[⛀]",
                 files = {
-                    "hercules.lua"
+                    "Herperides/hercules.lua"
                 }
             }
         }
@@ -49,66 +50,138 @@ local installationTree = {
                 type = "Turtle 🪓",
                 peripherals = "[⛀]",
                 files = {
-                    "thor.lua"
+                    "Yggdrasil/thor.lua"
                 }
             }
         }
     }
 }
 
-for folder, entry in pairs(installationTree) do
-    print(folder .. "/" .. " (" .. entry.info .. ")")
-    for name, programme in pairs(entry.children) do
-        print("  " .. name .. " (" .. programme.info .. ") " .. programme.peripherals)
+local selectedBotIndex = 1
+
+local function installSelected()
+    shell.run("set motd.enabled false")
+    fs.delete("startup.lua")
+    fs.delete("pulled/")
+    fs.makeDir("pulled")
+    shell.setDir("pulled/")
+
+    local bot = getBotByIndex(selectedBotIndex)
+
+    for file in bot.files do
+        shell.run("wget " .. repository .. file)
+    end
+
+    shell.run("cp " .. bot.files[1] .. " ../startup.lua")
+
+    shell.setDir("/")
+    fs.delete("installer.lua")
+    shell.run("reboot")
+end
+
+local function getBotByIndex(index)
+    local botIndex = 1
+
+    for folder, entry in pairs(installationTree) do
+        for name, programme in pairs(entry.children) do
+            if botIndex == index then
+                return programme
+            end
+
+            botIndex = botIndex + 1
+        end
     end
 end
 
-term.clear()
-
-local label = os.getComputerLabel()
-if label then
-    print("Running installer for: " .. label)
-else
-    print("Running installer for: " .. os.getComputerID())
+local function getAmountOfBots()
+    local numberOfBots = 0
+    for folder, entry in pairs(installationTree) do
+        for name, programme in pairs(entry.children) do
+            numberOfBots = numberOfBots + 1 
+        end
+    end
+    return numberOfBots
 end
 
-shell.run("set motd.enabled false")
-print("Removing junk")
-fs.delete("startup.lua")
-fs.delete("pulled/")
-fs.makeDir("pulled")
-shell.setDir("pulled/")
+while true do
+    term.setCursorPos(1, 1)
+    local event, key = os.pullEvent("key")
 
-if string.lower(label) == "gaia" then
-    print("Installation for Gaia")
-    print("Downloading files")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/gaia.lua")
-    shell.run("cp gaia.lua ../startup.lua")
+    if (
+        keys.getName(key) == "up" and
+        selectedBotIndex >= 1
+    ) then
+        selectedBotIndex = selectedBotIndex - 1
+    end
 
-    -- Create additional images directory with logos
-    shell.setDir("/")
-    fs.makeDir("images")
-    shell.setDir("images/")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/logo.nfp")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/logo2.nfp")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/punch.nfp")
-elseif string.lower(label) == "hercules" then
-    print("Installation for Hercules")
-    print("Downloading files")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Hercules/hercules.lua")
-    shell.run("cp hercules.lua ../startup.lua")
-elseif string.lower(label) == "apollo" then
-    print("Installation for Apollo")
-    print("Downloading files")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Apollo/apollo.lua")
-    shell.run("cp apollo.lua ../startup.lua")
-elseif string.lower(label) == "ares" then
-    print("Installation for Ares")
-    print("Downloading files")
-    shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Ares/ares.lua")
-    shell.run("cp ares.lua ../startup.lua")
+    if (
+        keys.getName(key) == "down" and
+        selectedBotIndex <= getAmountOfBots()
+    ) then
+        selectedBotIndex = selectedBotIndex + 1
+    end
+
+    if (keys.getName(key) == "enter") then
+        installSelected()
+    end
+
+    
+    for folder, entry in pairs(installationTree) do
+        local currentBotIndex = 1
+        print(folder .. "/" .. " (" .. entry.info .. ")")
+        for name, programme in pairs(entry.children) do
+            if currentBotIndex == selectedBotIndex then
+                write(">> ")
+            end
+            print(pretty.nest("  " .. name .. " (" .. programme.info .. ") " .. programme.peripherals))
+
+            currentBotIndex = currentBotIndex + 1
+        end
+    end
+
+    sleep(1)
 end
 
-shell.setDir("/")
-fs.delete("installer.lua")
-shell.run("reboot")
+
+-- local label = os.getComputerLabel()
+-- if label then
+--     print("Running installer for: " .. label)
+-- else
+--     print("Running installer for: " .. os.getComputerID())
+-- end
+
+
+
+-- if string.lower(label) == "gaia" then
+--     print("Installation for Gaia")
+--     print("Downloading files")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/gaia.lua")
+--     shell.run("cp gaia.lua ../startup.lua")
+
+--     -- Create additional images directory with logos
+--     shell.setDir("/")
+--     fs.makeDir("images")
+--     shell.setDir("images/")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/logo.nfp")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/logo2.nfp")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Gaia/images/punch.nfp")
+-- elseif string.lower(label) == "hercules" then
+--     print("Installation for Hercules")
+--     print("Downloading files")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Hercules/hercules.lua")
+--     shell.run("cp hercules.lua ../startup.lua")
+-- elseif string.lower(label) == "apollo" then
+--     print("Installation for Apollo")
+--     print("Downloading files")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Apollo/apollo.lua")
+--     shell.run("cp apollo.lua ../startup.lua")
+-- elseif string.lower(label) == "ares" then
+--     print("Installation for Ares")
+--     print("Downloading files")
+--     shell.run("wget https://raw.githubusercontent.com/ShinyRa/ComputerCraftScripts/main/Hesperides/Ares/ares.lua")
+--     shell.run("cp ares.lua ../startup.lua")
+-- end
+
+-- shell.setDir("/")
+-- fs.delete("installer.lua")
+-- shell.run("reboot")
